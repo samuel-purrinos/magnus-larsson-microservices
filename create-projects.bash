f6f17dd0c0997aae22b18d648915a710eca7c963
Samuel Purriños <samuel.purrinos@outlook.com>
2025-01-26 14:50:25 +0100

    Add  skeleton projects in a multiproject build

---
+++ create-projects.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+spring init --boot-version=3.3.0 --type=gradle-project --java-version=21 --packaging=jar --name=product-service --package-name=com.uichesoh.microservices.core.product --groupId=com.uichesoh.microservices.core.product --dependencies=actuator,webflux --version=1.0.0-SNAPSHOT product-service
+spring init --boot-version=3.3.0 --type=gradle-project --java-version=21 --packaging=jar --name=review-service --package-name=com.uichesoh.microservices.core.review --groupId=com.uichesoh.microservices.core.review --dependencies=actuator,webflux --version=1.0.0-SNAPSHOT review-service
+spring init --boot-version=3.3.0 --type=gradle-project --java-version=21 --packaging=jar --name=recommendation-service --package-name=com.uichesoh.microservices.core.recommendation --groupId=com.uichesoh.microservices.core.recommendation --dependencies=actuator,webflux --version=1.0.0-SNAPSHOT recommendation-service
+spring init --boot-version=3.3.0 --type=gradle-project --java-version=21 --packaging=jar --name=product-composite-service --package-name=com.uichesoh.microservices.composite.product --groupId=com.uichesoh.microservices.composite.product --dependencies=actuator,webflux --version=1.0.0-SNAPSHOT product-composite-service
+cd ..
